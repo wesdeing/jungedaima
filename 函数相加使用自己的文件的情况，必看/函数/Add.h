28b83488f0fc1a2b3ad44e6e4ae17de9908266d7
jun <2125756200@qq.com>
2022-07-23 17:07:07 +0800

    重要代码

---
+++ 函数相加使用自己的文件的情况，必看/函数/Add.h
@@ -0,0 +1,4 @@
+#ifndef __Add_H__
+# define __Add_H__
+int Add(int x, int y);
+#endif // !__Add__H__
---
+++ 函数相加使用自己的文件的情况，必看/函数/函数1.c
@@ -0,0 +1,9 @@
+# include<stdio.h>
+# include"Add.h"
+int main()
+{
+	int a = 10;
+	int b = 20;
+	int sum = Add(a, b);
+	printf("%d", sum);
+}
