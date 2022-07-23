a082e4e4c0e2460064e2bf12c409077c5215cacf
jun <2125756200@qq.com>
2022-07-23 17:10:55 +0800

    删除了部分代码

--- 函数相加使用自己的文件的情况，必看/函数/Add.h
+++
@@ -1,4 +0,0 @@
-#ifndef __Add_H__
-# define __Add_H__
-int Add(int x, int y);
-#endif // !__Add__H__
--- 函数相加使用自己的文件的情况，必看/函数/函数1.c
+++
@@ -1,9 +0,0 @@
-# include<stdio.h>
-# include"Add.h"
-int main()
-{
-	int a = 10;
-	int b = 20;
-	int sum = Add(a, b);
-	printf("%d", sum);
-}
